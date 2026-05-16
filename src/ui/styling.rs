6fcbcc0ad8638d4fd22e91bc0c70af395a4790a4
michael <nuns-pastes-0s@icloud.com>
2026-05-16 14:29:47 -0700

    reorganize code
    To make the code cleaner when adding the stylizing of the GUI, created an UI folder to hold the GUI along with the future necessary folders/Rust files.

---
+++ src/ui/styling.rs
@@ -0,0 +1,10 @@
+pub mod static_images {
+    pub static MAIN_MENU_IMAGE: &str = "icons/main_menu.png";
+    pub static PLAY_IMAGE: &str = "icons/play.png";
+    pub static PAUSE_IMAGE: &str = "icons/pause.png";
+    pub static FORWARD_IMAGE: &str = "icons/forward.png";
+    pub static BACKWARD_IMAGE: &str = "icons/backward.png";
+    pub static LOOP_IMAGE: &str = "icons/loop.png";
+    pub static SHUFFLE_IMAGE: &str = "icons/shuffle.png";
+    pub static VOLUME_IMAGE: &str = "icons/volume.png";
+}
