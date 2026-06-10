0e2aa33e6da4d8154789535c8d30180efad89c8e
michael <nuns-pastes-0s@icloud.com>
2026-06-10 09:57:33 -0700

    New icons and organized icons.rs file
    Added several new icons to the icons.rs file. Also organized the icons so it make it more clear which icons are for which area.

--- src/ui/styling/icons.rs
+++ src/ui/styling/icons.rs
@@ -1,8 +1,9 @@
+// Logo Icons
 pub static CLIPLAY_LOGO_GREY_ICON: &str = "icons/cliplay_logo_grey.png";
+
+// Video Player Control Icons
 pub static MAIN_MENU_CLOSED_ICON: &str = "icons/main_menu_closed.png";
 pub static MAIN_MENU_OPEN_ICON: &str = "icons/main_menu_open.png";
-pub static PLAYLISTS_ICON: &str = "icons/playlists_icon.png";
-pub static SELECT_SINGLE_VID_ICON: &str = "icons/select_single_vid.png";
 pub static PLAY_ICON: &str = "icons/play.png";
 pub static STOP_ICON: &str = "icons/stop.png";
 pub static PAUSE_ICON: &str = "icons/pause.png";
@@ -15,3 +16,13 @@ pub static LOOP_ONE_ICON: &str = "icons/loop_single.png";
 pub static LOOP_INFINITE_ICON: &str = "icons/loop_infinite.png";
 pub static SHUFFLE_ICON: &str = "icons/shuffle.png";
 pub static VOLUME_ICON: &str = "icons/volume.png";
+
+// Main Menu Icons
+pub static PLAYLISTS_ICON: &str = "icons/playlists_icon.png";
+pub static SELECT_SINGLE_VID_ICON: &str = "icons/select_single_vid.png";
+
+//  Playlist menu Icons
+pub static NEW_PLAYLIST_ICON: &str = "icons/new_playlist_icon.png";
+pub static EDIT_PLAYLIST_ICON: &str = "icons/edit_playlist_icon.png";
+pub static DELETE_PLAYLIST_ICON: &str = "icons/delete_playlist_icon.png";
+pub static PLAY_PLAYLIST_ICON: &str = "icons/play_playlist_icon.png";
